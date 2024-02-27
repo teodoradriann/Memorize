565f21215a73b9adc1279595bd3b9a5e7d8cea58
papillon107 <teodormiron75@gmail.com>
2024-02-27 20:02:17 +0200

    finished the app like in the first 2 lectures

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,38 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis: [String] = ["🦭", "🦋", "🐌", "🦖", "🦇", "🐕", "🦫"]
-    @State var cardCounter = 3
+    let emojis: [String] = ["🦭", "🦋", "🐌", "🦖", "🦇", "🐕", "🦫", "🐝", "🦎", "🦘", "🐀"]
+    @State var cardCounter = 4
     
     var body: some View {
-        ScrollView {
-            cards
+        VStack {
+            ScrollView{
+                cards
+            }
             Spacer()
             cardsAdjusters
         }
         .padding()
+        .background(Color.gray)
     }
     
     var cards: some View {
-        HStack {
+        LazyVGrid(columns: [GridItem(.adaptive (minimum: 120))]) {
             ForEach(0 ..< cardCounter, id: \.self) { index in
-                CardView(content: emojis[index])
+                CardView(emoji: emojis[index]).aspectRatio(contentMode: .fit)
             }
-        }.foregroundStyle(.brown)
+        }.foregroundStyle(.quaternary)
     }
     
+    //functie care ajusteaza numarul cardurilor conform unui numar si returneaza un buton
     func adjustCards(by number: Int, nameOfAdjuster: String) -> some View {
         Button(action: {
             cardCounter += number
         }, label: {
             Image(systemName: nameOfAdjuster)
+                .font(.largeTitle)
+                .foregroundStyle(.white)
         }).disabled(cardCounter + number < 1 || cardCounter + number > emojis.count)
+    }
+    
+    var buttonsBackround: some View {
+        Rectangle()
     }
     
     var cardsAdjusters: some View {
         HStack{
             cardAdder
-            Spacer()
+            Spacer().frame(maxWidth: 150)
             cardRemover
         }
     }
@@ -52,22 +62,27 @@ struct ContentView: View {
         adjustCards(by: -1, nameOfAdjuster: "minus")
     }
 }
+
+// structura de tip View care imi face un card, are 2 proprietati: emojiul si daca este sau nu intoarsa
 struct CardView: View {
-    let content: String
-    @State var isFacedUp = true
+    let emoji: String
+    @State var isFacedUp = true // @State e un fel de pointer care ma lasa sa modific variabila
     
     var body: some View {
-        let base = RoundedRectangle(cornerRadius: 10)
-        
+        let base = RoundedRectangle(cornerRadius: 40) // baza, un dreptunghi cu corner radius de 40
+        // ZStackul adica 2 dreptunghiuri suprapuse, unul alb si celalat peste el care e borderul
+        // apoi Emojiul
+        // opacitatea o setez daca e sau nu cu fata in sus
         ZStack {
             Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 3)
-                Text(content).font(.largeTitle)
+                Text(emoji).font(.largeTitle)
             }
             .opacity(isFacedUp ? 1 : 0)
             base.fill().opacity(isFacedUp ? 0 : 1)
-        }.onTapGesture {
+        }
+        .onTapGesture {
             isFacedUp.toggle()
         }
     }
